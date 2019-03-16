b6371703dbfd5b8fa66b1bf33f2cb6bef9223300
Mathias Pius <contact@pius.io>
2019-03-16 17:25:01 +0100

    Cleaned up python code a bit and added checks to instances deduce

--- orca/__main__.py
+++ orca/__main__.py
@@ -1,13 +1,67 @@
+"""Orca.
+
+Usage:
+  orca instances deduce [-y]
+  orca instances export
+  orca provision
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+"""
+
 import sys
 import json
 import subprocess
+import os
+import tempfile
+from shutil import copyfile
+from docopt import docopt
 
 
-if len(sys.argv) <= 1:
-    print("unknown command")
-else:
-    if(sys.argv[1] == "hosts"):
-        if(sys.argv[2] == "refresh"):
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Orca 0.1')
+
+    if(arguments['instances']):
+        if(arguments['deduce']):
+            instances = os.listdir("instances")
+            with tempfile.NamedTemporaryFile('w') as file:
+                for instance in instances:
+                    file.write("module \"{0}\" {{ source = \"../instances/{0}\" }}\n".format(instance))
+                    
+                file.write("\n")
+                file.write("output \"hosts\" {\n" )
+                file.write("  value = [\n")
+
+                for instance in instances:
+                    file.write("  {\n")
+                    file.write("    ssh_key = \"${{module.{}.ssh_key}}\"\n".format(instance))
+                    file.write("    address = \"${{module.{}.ipv4}}\"\n".format(instance))
+                    file.write("  },\n")
+
+                file.write("  ]\n")
+                file.write("}")
+                file.flush()
+
+                if(os.path.isfile("terraform/instances.tf")):
+                    print("showing diff of old (left) and new (right) file\n")
+                    print("--@@@--")
+                    subprocess.run(["diff", "-y", "terraform/instances.tf", file.name])
+                    print("\n--@@@--\n")
+                    if(arguments['-y'] or input("\nreplace old (left) instances.tf with new (right)? [y/N]: ") == "y"):
+                        os.remove("terraform/instances.tf")
+                        copyfile(file.name, "terraform/instances.tf")
+                        print("wrote {} instances to terraform/instances.tf".format(len(instances)))
+                    else:
+                        print("deduced instances ignored, no changes made.")
+                else:
+                    copyfile(file.name, "terraform/instances.tf")
+                    print("wrote {} instances to terraform/instances.tf".format(len(instances)))
+
+        elif(arguments['export']):
             output = json.loads(subprocess.check_output(["terraform", "output", "-json"]))
             hosts = output['hosts']['value']
 
@@ -16,11 +70,9 @@ else:
                 for host in hosts:
                     file.write("{} ansible_ssh_private_key_file={}".format(host['address'], host['ssh_key']))
             
-            print("wrote {} host(s) to ansible/hosts".format(len(hosts)))
-        else:
-            print("unknown hosts command: {}".format(sys.argv[2]))
-    elif(sys.argv[1] == "provision"):
+            print("exported {} instances to ansible/hosts".format(len(hosts)))
+    elif(arguments['provision']):
         subprocess.run(["terraform", "init"], capture_output=True)
         subprocess.run(["terraform", "apply"])
     else:
-        print("unknown command: {}".format(sys.argv[1]))
+        print(arguments)
